7cb996e08a273ae9fd2f61810513e3d52e304b81
Robinson <robinson@dorkbox.com>
2022-02-02 16:19:31 +0100

    updated build dependencies + license info

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,10 +19,10 @@ import java.time.Instant
 gradle.startParameter.showStacktrace = ShowStacktrace.ALWAYS   // always show the stacktrace!
 
 plugins {
-    id("com.dorkbox.GradleUtils") version "2.9"
-    id("com.dorkbox.Licensing") version "2.9"
-    id("com.dorkbox.VersionUpdate") version "2.3"
-    id("com.dorkbox.GradlePublish") version "1.11"
+    id("com.dorkbox.GradleUtils") version "2.16"
+    id("com.dorkbox.Licensing") version "2.12"
+    id("com.dorkbox.VersionUpdate") version "2.4"
+    id("com.dorkbox.GradlePublish") version "1.12"
 
     kotlin("jvm") version "1.5.21"
 }
